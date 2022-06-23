d77cc6fc72cebc7835f94c11d3050f94d922f3d9
the-supernova <biswalabhijit04@gmail.com>
2022-06-23 12:27:31 +0530

    Connect emoji dataset / Add input element

--- src/App.js
+++ src/App.js
@@ -1,8 +1,47 @@
 import React, { useState } from "react";
 import './App.css';
+import appbasejs from "appbase-js";
+
+var appbaseRef = appbasejs({
+  url: "https://appbase-demo-ansible-abxiydt-arc.searchbase.io",
+  app: "emoji-dataset",
+  credentials: "f1da7b624918:3331c67d-3477-4b24-aa89-aefc6ca4683e"
+});
+
 
 function App() {
   const [searchText, setSearchText] = useState("");
+  // indicates whether api has responded or not
+  const [isSearching, setIsSearching] = useState(false);
+  // keeping record of the fetched results
+  const [results, setResults] = useState(null);
+
+  // makes a call to the backend to fetch results
+  const makeApiCall = () => {
+    setIsSearching(true);
+    const SEARCH_ID = "emoji_search";
+    appbaseRef
+      .reactiveSearch(
+        [
+          {
+            id: SEARCH_ID,
+            size: 10,
+            ...(searchText && { value: searchText })
+          }
+        ],
+        {
+          enableQueryRules: true
+        }
+      )
+      .then((res) => {
+        setIsSearching(false);
+        setResults(res[SEARCH_ID].hits.hits);
+      })
+      .catch((err) => {
+        console.log("search error: ", err);
+      });
+  };
+
   return (
     <div className="app-root">
       <div className="input-wrapper">
@@ -13,6 +52,9 @@ function App() {
           value={searchText}
           placeholder="Try searching, 'man swimming' "
         />
+        <button id="recommend-btn" onClick={makeApiCall}>
+          Recommend
+        </button>
       </div>
     </div>
   );
